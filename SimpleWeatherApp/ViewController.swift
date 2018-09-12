49d1153eb9c6e699ed378e20c60795d037dbaf22
Halil Özel <halilozel1903@gmail.com>
2018-09-12 19:09:15 +0300

    proje olusturuluyor

--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var minMaxLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func endEditingTextField(_ sender: UITextField) {
+        
     }
-
-
+    
+    
+    @IBAction func pressTodayButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    @IBAction func pressTomorrowButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
 }
 
